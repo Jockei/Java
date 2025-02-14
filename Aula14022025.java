ce30fb8d6eee273f681b185af35dae40370b7e52
Jockei <159291450+Jockei@users.noreply.github.com>
2025-02-14 08:51:15 -0300

    Add files via upload

---
+++ Aula14022025.java
@@ -0,0 +1,13 @@
+package com.mycompany.aula14022025;
+
+public class Aula14022025 {
+
+    public static void main(String[] args) {
+        System.out.println("menu!");
+        System.out.println("1 soma");
+        System.out.println("2 subtração");
+        System.out.println("3 multiplicação");
+        System.out.println("4 divisão");
+        System.out.println("0 sair");
+    }
+}
