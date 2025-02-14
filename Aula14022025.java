cada62185f0a21c10c213c85ccbd0f5857b58057
Jockei <159291450+Jockei@users.noreply.github.com>
2025-02-14 08:52:29 -0300

    Update Aula14022025.java

--- Aula14022025.java
+++ Aula14022025.java
@@ -1,13 +1,25 @@
 package com.mycompany.aula14022025;
 
+import java.util.Scanner;
+
 public class Aula14022025 {
 
     public static void main(String[] args) {
-        System.out.println("menu!");
-        System.out.println("1 soma");
-        System.out.println("2 subtração");
-        System.out.println("3 multiplicação");
-        System.out.println("4 divisão");
-        System.out.println("0 sair");
+        
+        Scanner scan = new Scanner(System.in);
+        int opcao;
+        
+        System.out.println("\t=========================");
+        System.out.println("\t\tmenu!");
+        System.out.println("\t=========================");
+        System.out.println("\t(1) soma (+)");
+        System.out.println("\t(2) subtração (-)");
+        System.out.println("\t(3) multiplicação (*)");
+        System.out.println("\t(4) divisão (/)");
+        System.out.println("\t(0) sair");
+        System.out.println("\t=========================");
+        System.out.println("\tDigite a opção desejada: ");
+        opcao = scan.nextInt();
+        System.out.printf("A opção selecinada foi: %d \n", opcao);
     }
 }
