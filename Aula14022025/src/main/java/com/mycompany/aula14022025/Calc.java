493a5f6285b465804fb83f9791c5f1f6425694e7
Jockei <159291450+Jockei@users.noreply.github.com>
2025-03-07 07:29:04 -0300

    Add files via upload

---
+++ Aula14022025/src/main/java/com/mycompany/aula14022025/Calc.java
@@ -0,0 +1,19 @@
+package com.mycompany.aula14022025;
+
+public class Calc {
+        public static int somar(int a, int b) {
+        return a + b;
+    }
+    
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
+    
+    public static double divisao(double a, double b) {
+        return a / b;
+    }
+    
+    public static int multiplicacao(int a, int b) {
+        return a * b;
+    }
+}
